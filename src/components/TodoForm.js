a4fc71527f1ba83e59ea6cc6ebb0d0b922abcc79
Oliver van den Bosch <74139069+ovandenbosch@users.noreply.github.com>
2021-03-01 13:46:26 -0500

    Adding css

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
 
   const inputRef = useRef(null);
 
@@ -26,17 +26,36 @@ function TodoForm(props) {
 
   return (
     <div>
-      <form className="todo-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add a todo"
-          value={input}
-          name="text"
-          className="todo-input"
-          onChange={handleChange}
-          ref={inputRef}
-        />
-        <button className="todo-button">Add todo</button>
+      <form onSubmit={handleSubmit} className="todo-form">
+        {props.edit ? (
+          <>
+            <input
+              placeholder="Update your item"
+              value={input}
+              onChange={handleChange}
+              name="text"
+              ref={inputRef}
+              className="todo-input edit"
+            />
+            <button onClick={handleSubmit} className="todo-button edit">
+              Update
+            </button>
+          </>
+        ) : (
+          <>
+            <input
+              placeholder="Add a todo"
+              value={input}
+              onChange={handleChange}
+              name="text"
+              className="todo-input"
+              ref={inputRef}
+            />
+            <button onClick={handleSubmit} className="todo-button">
+              Add todo
+            </button>
+          </>
+        )}
       </form>
     </div>
   );
